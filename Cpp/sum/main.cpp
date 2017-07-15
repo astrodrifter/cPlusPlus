49a5e5b6e97a930e579a41faea28d5818b9afba5
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-07-15 23:32:42 +1000

    while(std::can >> value) sum += value

--- Cpp/sum/main.cpp
+++ Cpp/sum/main.cpp
@@ -3,9 +3,33 @@
 
 int main(int argc, char **argv)
 {
-	std::cout << "Enter two values\n" << std::endl;
+	/*std::cout << "Enter two values\n" << std::endl;
     int v1 = 0, v2 = 0;
     std::cin >> v1 >> v2;
-    std::cout << "The sum of " << v1 << " and " << v2 << " = " << (v1+v2) << ".\n" << std::endl;
+    std::cout << "The sum of " << v1 << " and " << v2 << " = " 
+    << (v1+v2) << ".\n" << std::endl;
+    
+    std::cout << "hello world\n"; */
+    
+    /* sum 1 to 10 */
+    /*int sum = 0, val = 1;
+    while(val <= 10){
+        sum += val;
+        ++val;
+    }
+    std:: cout << "The sum of 1 to 10 = " << sum << std::endl;*/
+    
+    /* when you dont know how many number are to be summed */
+    /*int value = 0, sum = 0;
+    while(std:: cin >> value)
+        sum += value;
+    std::cout << "The sum = " << sum << std::endl;*/
+    
+    std:: cout << "Enter your number to be summed with spaces and 'q' to finish:\n";
+    int sum = 0, value = 0;
+    // read until end-of-file, calculating a running total of all values read
+    while (std::cin >> value)
+        sum += value; // equivalent to sum = sum + value 
+        std::cout << "Sum is: " << sum << std::endl; 
 	return 0;
 }
