007d604703d52610c0de5be7fc730b5cb4a297d0
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-07-16 22:14:26 +1000

    extract ints from strings

--- Cpp/Q3new/main.cpp
+++ Cpp/Q3new/main.cpp
@@ -6,22 +6,31 @@
 static int getLength(char * str);
 static int getArr1Len(char * str);
 static int getArr2Len(char * str, int arr1Len);
+static void fillArray1(char * str, int arr1[100], int arr1Len);
+static void fillArray2(char * str, int arr2[100], int arr2Len);
+static int findOperator(char * str, int length);
 
 
 int main(int argc, char **argv)
 {
     /* get data */
 	std:: cout << "Enter you string with 'q' to finish:\n";
-    int sum = 0, length = 0, arr1Len = 0, arr2Len = 0, arr1[10], arr2[10];
+    int length = 0, arr1Len = 0, arr2Len = 0, op = 0;
     char myString[50];
     std::cin >> myString; // get input
     length = getLength(myString); // get length of string
     arr1Len = getArr1Len(myString); // get length for array 1
     arr2Len = getArr2Len(myString, arr1Len); // get length for array 2
+    //op = findOperator(myString, length);
 
     /* enter ints into arrays */
+    int arr1[arr1Len], arr2[arr2Len];
+    fillArray1(myString, arr1, arr1Len);
+    
+    
     std::cout << "String length = " << length << "\narray 1 length = "
-    << arr1Len << "\narray 2 length = " << arr2Len << std::endl;
+    << arr1Len << "\narray 2 length = " << arr2Len << "\n operator is " 
+    << myString[op] << " at location " << op << std::endl;
     
 	return 0;
 }
@@ -58,3 +67,22 @@ static int getArr2Len(char * str, int arr1Len) {
     }
     return length;
 }
+/* find operator */
+static int findOperator(char * str, int length){
+    int i = 0, location;
+    while(str[i] != '\0'){
+        if(str[i] < 41 && str[i] < 48){
+            location = str[i];
+        } 
+        i++;  
+    }
+    return location;
+}
+/* fill int array 1 */
+/* static void fillArray1(char * str, int arr1[100], int arr1Len){
+    for(int i = 0; i < a)
+}*/
+/* fill int array 2 */
+/*static void fillArray2(char * str, int arr2[100], int arr2Len){
+    
+}*/
