59a7a9bb14ec0565c6c510bb2da1fc5e5bcd94b4
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-07-21 21:12:45 +1000

    building. not solved.

--- Cpp/replaceString/main.cpp
+++ Cpp/replaceString/main.cpp
@@ -6,8 +6,36 @@ using namespace std;
 
 void replace(string string1, string string2, string string3){
     string newString;
+    
+    // string lensths
+    int len2, len3;
+    len2 = string2.length();
+    len3 = string3.length();
+    
     //find string2 in string 1 and replace with string3
     
+    // find string2 in string1
+    int i = 0, j = 0, position = 0;
+    while(string1[i] != '\0'){
+        if(sameString(string1,string2,i)){
+            position = i;
+            cout << "found at " << i << "\n";
+        }
+    }
+    
+    
+}
+
+int sameString(string str1, string str2, int index){
+    int len1, len2, j = 0;
+    string temp;
+    len1 = str1.length();
+    len2 = str2.length();
+    for(int i = index; i < len2; i++){
+        temp[j] = 
+        
+    }
+    return 1;
 }
 
 int main(int argc, char **argv)
